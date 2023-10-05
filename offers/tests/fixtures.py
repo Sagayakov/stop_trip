c6d0ff22fc83641e7b131775f46a8fc9098c1754
Grisha Malanchuk <grishamalanchuk@gmail.com>
2023-10-05 19:09:06 +0300

    feat(back): tests for create transport

--- offers/tests/fixtures.py
+++ offers/tests/fixtures.py
@@ -1,8 +1,27 @@
-# import pytest
-#
-# from offers.models import Advertisement, PropertyAmenity
-#
-#
+import json
+
+import pytest
+
+
+@pytest.fixture
+def jwt_token(api_client, django_user_model):
+    email = 'user@gmail.com'
+    password = 'password'
+
+    django_user_model.objects.create_user(
+        email=email,
+        password=password
+    )
+
+    response = api_client.post(
+        path='/api/auth/jwt/create/',
+        data=json.dumps({'email': email, 'password': password}),
+        content_type='application/json'
+    )
+
+    return response.data['access']
+
+
 # @pytest.fixture
 # def property_advertisement():
 #     property_amenities1 = PropertyAmenity.objects.create(name='test1')
