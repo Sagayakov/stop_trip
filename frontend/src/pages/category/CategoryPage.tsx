a5955c82bec6bf918e4948d8d5ca14dbce4fe743
Semyon Konevskiy <konevskiy7on@gmail.com>
2023-10-16 13:29:21 +0300

    made adaptive for form(425-767)

--- frontend/src/pages/category/CategoryPage.tsx
+++ frontend/src/pages/category/CategoryPage.tsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import './style/category-page.scss';
 import './style/1024-1439-category-page.scss'
 import './style/768-1023-category-page.scss'
+import './style/425-767-category-page.scss'
 import { AnyCategory, SettingAdvertsForm } from '../../widgets/index';
 import { Pagination } from '../../features/pagination';
 
@@ -21,7 +22,7 @@ export const CategoryPage = () => {
             <h1>{description}</h1>
             <div className="filters-adverts">
                 <SettingAdvertsForm />
-                <AnyCategory />
+                {/* <AnyCategory /> */}
             </div>
             <Pagination />
         </>
