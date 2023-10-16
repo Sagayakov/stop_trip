cfe863a8439e81a5a7ba72e2663b8c94ce814a38
Semyon Konevskiy <konevskiy7on@gmail.com>
2023-10-16 16:41:14 +0300

    made a card 425-767

--- frontend/src/pages/category/CategoryPage.tsx
+++ frontend/src/pages/category/CategoryPage.tsx
@@ -7,22 +7,48 @@ import './style/768-1023-category-page.scss'
 import './style/425-767-category-page.scss'
 import { AnyCategory, SettingAdvertsForm } from '../../widgets/index';
 import { Pagination } from '../../features/pagination';
+import { useEffect, useState } from 'react';
+import { ArrowLeft14x7 } from '../../shared/ui/icons/icons-tools/ArrowLeft14x7';
 
 export const CategoryPage = () => {
     const category = location.pathname.slice(1);
     const description = categories[category].description;
+    const [width, setWidth] = useState<number>(window.innerWidth)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+    }, [window.innerWidth])
 
     return (
         <>
             <Controls />
-            <div className="bread-crumbs">
-                <NavLink to="/">Главная</NavLink>
-                {` > ${description}`}
-            </div>
-            <h1>{description}</h1>
+            {width > 475 ? (
+                <>
+                    <div className="bread-crumbs">
+                        <NavLink to="/">Главная</NavLink>
+                        {` > ${description}`}
+                    </div>
+                    <h1>{description}</h1>
+                </>
+            ) : (
+                <>
+                    <div className="bread-crumbs">
+                        <NavLink to="/">
+                            <ArrowLeft14x7 color='' style={{
+                                cursor: 'pointer',
+                                marginRight: '16px'
+                            }}/>
+                        </NavLink>
+
+                    </div>
+                </>
+            )}
             <div className="filters-adverts">
                 <SettingAdvertsForm />
-                {/* <AnyCategory /> */}
+                <AnyCategory />
             </div>
             <Pagination />
         </>
