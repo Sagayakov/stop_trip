c808cd4d3cfbf56b1d00c9efed90d2f1d0ba1d95
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-26 19:27:57 +0200

    fix: fix request for resetting password

--- frontend/src/features/header/modal/modalResetPassword/ModalResetPassword.tsx
+++ frontend/src/features/header/modal/modalResetPassword/ModalResetPassword.tsx
@@ -1,33 +1,36 @@
-import { useState } from "react"
-import { useForm, SubmitHandler } from "react-hook-form"
-import './ModalResetPassword.scss'
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import './ModalResetPassword.scss';
 
 export const ModalResetPassword = () => {
-    interface Email{
-        email: string
+    interface Email {
+        email: string;
     }
-    const url = import.meta.env.VITE_BASE_URL
-    const { register, handleSubmit, formState } = useForm<Email>()
-    const {errors} = formState
-    const [success, setSuccess] = useState(false)
+    const url = import.meta.env.VITE_BASE_URL;
+    const { register, handleSubmit, formState } = useForm<Email>();
+    const { errors } = formState;
+    const [success, setSuccess] = useState(false);
 
     const onsubmit: SubmitHandler<Email> = async (data) => {
-        const body = data.email
-        console.log(body)
+        const { email } = data;
+        console.log(email);
         try {
-            const response = await fetch(`${url}/api/auth/users/reset_password/`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body),
-            });
-            console.log(response)
-            if(response.ok) setSuccess(true)
+            const response = await fetch(
+                `${url}/api/auth/users/reset_password/`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email }),
+                }
+            );
+            console.log(response);
+            if (response.ok) setSuccess(true);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
     return (
         <div className="form-reset-password">
@@ -35,36 +38,38 @@ export const ModalResetPassword = () => {
                 Пожалуйста, введите вашу почту. На нее придет ссылка для
                 восстановления пароля.
             </h3>
-            {success
-                ? <p>письмо для восстановления пароля отправлено на ваш почтовый ящик</p>
-                : (
-                    <form onSubmit={handleSubmit(onsubmit)}>
-                        <input
-                            {...register('email', {
-                                required: true,
-                                pattern:
-                                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                                minLength: 10,
-                            })}
-                            placeholder="Email"
-                            autoComplete="username"
-                            style={{
-                                border: `1px solid ${
-                                    errors?.email ? '#FF3F25' : '#DCDCDC'
-                                }`,
-                            }}
-                        />
-                        <div className="input-error">
-                            {errors?.email && (
-                                <p style={{ color: '#FF3F25', fontSize: '13px' }}>
-                                    Введите корректный email
-                                </p>
-                            )}
-                        </div>
-                        <input type="submit" value="Отправить" />
-                    </form>
-                )
-            }
+            {success ? (
+                <p>
+                    письмо для восстановления пароля отправлено на ваш почтовый
+                    ящик
+                </p>
+            ) : (
+                <form onSubmit={handleSubmit(onsubmit)}>
+                    <input
+                        {...register('email', {
+                            required: true,
+                            pattern:
+                                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                            minLength: 10,
+                        })}
+                        placeholder="Email"
+                        autoComplete="username"
+                        style={{
+                            border: `1px solid ${
+                                errors?.email ? '#FF3F25' : '#DCDCDC'
+                            }`,
+                        }}
+                    />
+                    <div className="input-error">
+                        {errors?.email && (
+                            <p style={{ color: '#FF3F25', fontSize: '13px' }}>
+                                Введите корректный email
+                            </p>
+                        )}
+                    </div>
+                    <input type="submit" value="Отправить" />
+                </form>
+            )}
         </div>
     );
-}
+};
