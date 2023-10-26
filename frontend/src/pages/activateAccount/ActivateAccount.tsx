799f186b2d4e38634c1f4564ce898ec3af6b1e11
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-26 11:12:08 +0200

    refactor: move func to separate file, add bottom margin

--- frontend/src/pages/activateAccount/ActivateAccount.tsx
+++ frontend/src/pages/activateAccount/ActivateAccount.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import './ActivateAccount.scss';
+import { activate } from './api/activate';
 
 export const ActivateAccount = () => {
-    const url = import.meta.env.VITE_BASE_URL;
     const { uid, token } = useParams();
     const [success, setSuccess] = useState<boolean>(false);
 
@@ -12,13 +12,7 @@ export const ActivateAccount = () => {
             uid,
             token,
         };
-        const response = await fetch(`${url}/api/auth/users/activation/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
+        const response = await activate(body);
         if (response.ok) setSuccess(true);
     };
 
@@ -32,7 +26,7 @@ export const ActivateAccount = () => {
                 <h1>Активация аккаунта</h1>
                 {success
                     ? 'Ваш аккаунт активирован, спасибо за регистрацию на нашем сайте!'
-                    : 'еще нет'}
+                    : 'Ваш аккаунт еще не активирован'}
                 <div className="redirect">
                     <NavLink to={'/'}>Вернуться на главную</NavLink>
                 </div>
