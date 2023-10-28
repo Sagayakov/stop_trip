5a3269b12b95f37da5cd136d447b07b25ec32b8b
Semyon Konevskiy <konevskiy7on@gmail.com>
2023-10-28 12:28:29 +0300

    fix: corrected the loading display when changing the password, removed margin in header

--- frontend/src/features/formResetPassword/FormConfirmResetPassword.tsx
+++ frontend/src/features/formResetPassword/FormConfirmResetPassword.tsx
@@ -6,9 +6,10 @@ import { InputRepeatPassword } from './inputs/InputRepeatPassword';
 import { InputSubmit } from './inputs/InputSubmit';
 import { confimResetPassword } from './api/confirmResetPassword';
 import { NavLink, useParams } from 'react-router-dom';
-import { useAppDispatch } from '../../app/store/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/store/hooks';
 import { Dispatch } from '@reduxjs/toolkit';
 import { setLoading } from '../../entities/loading/model/setLoadingSlice';
+import { LoadingWithBackground } from '../../entities/loading/LoadingWithBackground';
 
 export const FormConfirmResetPassword = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -23,24 +24,26 @@ export const FormConfirmResetPassword = () => {
     const { uid, token } = useParams();
     const [success, setSuccess] = useState<boolean>(false);
     const dispatch: Dispatch = useAppDispatch()
+    const load = useAppSelector((state) => state.setLoading.loading)
 
     const onsubmit: SubmitHandler<ResetPasswordType> = async (submitData) => {
         if (uid && token) {
-            dispatch(setLoading(true))
+            await dispatch(setLoading(true))
             const response = await confimResetPassword({
                 new_password: submitData.password,
                 uid,
                 token,
             });
             if (response.ok){
-                setSuccess(true);
-                dispatch(setLoading(false))
+                await setSuccess(true);
+                await dispatch(setLoading(false))
             }
         }
     };
 
     return (
         <div className="reset">
+            {load && <LoadingWithBackground />}
             {!success ? (
                 <form
                     className="form"
