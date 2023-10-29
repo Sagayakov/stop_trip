e7fd49155521bb71872eabe37f145419830c4fb2
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-29 10:39:47 +0100

    feat: add base template for google auth

--- frontend/src/features/header/modal/modalEnter/FormEnter.tsx
+++ frontend/src/features/header/modal/modalEnter/FormEnter.tsx
@@ -13,6 +13,9 @@ import { submitEntForm } from './libr/submitEntForm';
 import { setIsResetPasswordModalOpen } from '../../../../features/header/model/modalAuth/reducers/isResetPasswordModalOpen';
 import { toggleModalEnter } from '../../../../features/header/model/modalAuth/reducers/toggleModal';
 import { setLoading } from '../../../../entities/loading/model/setLoadingSlice';
+import { useGoogleLogin } from '@react-oauth/google';
+import { setIsAuth } from '../../../../features/header/model/modalAuth/reducers/auth';
+import { url } from '../../../../shared/const/url';
 
 export const FormEnter = () => {
     const [togglePass, setTogglePass] = useState(false);
@@ -22,17 +25,33 @@ export const FormEnter = () => {
         });
     const dispatch = useAppDispatch();
 
-
     const onsubmit: SubmitHandler<AuthData> = async (submitData) => {
-        await dispatch(setLoading(true))
+        await dispatch(setLoading(true));
         await submitEntForm(submitData, dispatch, reset);
-        await dispatch(setLoading(false))
+        await dispatch(setLoading(false));
     };
 
     const openResetPasswordModal = () => {
         dispatch(toggleModalEnter(false));
         dispatch(setIsResetPasswordModalOpen(true));
     };
+
+    const loginWithGoogle = useGoogleLogin({
+        onSuccess: async (tokenResponse) => {
+            console.log(tokenResponse);
+            const { code: jwt } = tokenResponse;
+            const decodedJWT = await fetch(`${url}/api/auth/jwt/verify/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ token: jwt }),
+            });
+
+            if (decodedJWT.ok) dispatch(setIsAuth(true));
+        },
+        flow: 'auth-code',
+    });
 
     return (
         <form
@@ -54,7 +73,13 @@ export const FormEnter = () => {
             <InputSubmit />
             <div className="enter-with">
                 Войти с помощью
-                <div className="google">
+                <div
+                    className="google"
+                    onClick={() => {
+                        dispatch(setIsAuth(true));
+                        return loginWithGoogle();
+                    }}
+                >
                     <Google />
                 </div>
                 <div className="vk">
