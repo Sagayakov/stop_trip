3a2c8aee137b0810de574fd7e43e75ec406910bd
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-10-29 10:39:47 +0100

    feat: add base template for google auth

--- frontend/src/app/cookie/cookieAuth.ts
+++ frontend/src/app/cookie/cookieAuth.ts
@@ -1,8 +1,13 @@
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
-export const saveTokensAuthToCookie = ( accessToken?: string, refreshToken?: string ) => {
-    if (accessToken) Cookies.set('access_token', accessToken, { expires: 1/100 }); //срок действия в днях
-    if (refreshToken) Cookies.set('refresh_token', refreshToken, { expires: 15 }); //хранить в httpOnly cookie (хз как передать в тело запроса, чтобы получить новый access)
+export const saveTokensAuthToCookie = (
+    accessToken?: string,
+    refreshToken?: string
+) => {
+    if (accessToken)
+        Cookies.set('access_token', accessToken, { expires: 1 / 100 }); //срок действия в днях
+    if (refreshToken)
+        Cookies.set('refresh_token', refreshToken, { expires: 15 }); //хранить в httpOnly cookie (хз как передать в тело запроса, чтобы получить новый access)
 };
 /*
 1/1440 - 1 минута
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import './app/style/index.scss';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <App />
+    </GoogleOAuthProvider>
+);
