bb002b23ad8111c745a51a581377b2775830ba15
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-02 11:31:28 +0100

    fix: fix imports

--- frontend/src/features/LastAdverts/LastAdverts.tsx
+++ frontend/src/features/LastAdverts/LastAdverts.tsx
@@ -1,13 +1,13 @@
 import { useGetAdvertsQuery } from '../../app/api/fetchAdverts';
 import { LastAdvertsTypes } from '../../app/api/types/lastAdvertsTypes';
-import { Cart } from '../../entities/lastAdverts';
+import { Cart } from '../../entities/lastAdverts/Cart';
 import { LoadingWithBackground } from '../../entities/loading/LoadingWithBackground';
 import { Pagination } from '../pagination';
 import './libr/lastAdverts.scss';
 
 export const LastAdverts = () => {
     const { data = [], isLoading } = useGetAdvertsQuery('');
-    const reverseData = JSON.parse(JSON.stringify(data));//наверное стоит делать reverse массива на бэке и сразу получать в обратном порядке
+    const reverseData = JSON.parse(JSON.stringify(data)); //наверное стоит делать reverse массива на бэке и сразу получать в обратном порядке
 
     return (
         <div className="last-adverts">
