454fc6507accbe47759b04a34485d34e0b74585c
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-03 09:55:48 +0100

    fix: fix folder name

--- frontend/src/app/api/fetchAdverts.ts
+++ frontend/src/app/api/fetchAdverts.ts
@@ -10,19 +10,20 @@ export const fetchAdverts = createApi({
         getAdverts: build.query<LastAdvertsTypes[], string>({
             //дженериками передаем тип того что собираемся получить, а второй тип это то что передаем в качестве параметра при вызове хука, в данном случае пустая строка
             query: () => 'api/advertisements/',
-            providesTags: (result) => result // понадобится когда можно будет добавлять объявления
-                ? [
-                      ...result.map(({ id }) => ({
-                          type: 'Adverts' as const,
-                          id,
-                      })),
-                        { type: 'Adverts', id: 'LIST' },
-                  ]
-                : [{ type: 'Adverts', id: 'LIST' }],
+            providesTags: (result) =>
+                result // понадобится когда можно будет добавлять объявления
+                    ? [
+                          ...result.map(({ id }) => ({
+                              type: 'Adverts' as const,
+                              id,
+                          })),
+                          { type: 'Adverts', id: 'LIST' },
+                      ]
+                    : [{ type: 'Adverts', id: 'LIST' }],
         }),
         addAdvert: build.mutation<LastAdvertsTypes[], LastAdvertsTypes>({
             query: (body) => ({
-                url: 'api/advertisements/',// сюда вписать адрес для добавления нового объявления
+                url: 'api/advertisements/', // сюда вписать адрес для добавления нового объявления
                 method: 'POST',
                 body,
             }),
