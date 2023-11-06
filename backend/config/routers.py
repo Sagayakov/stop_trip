ddb84861ead6de176eeec743a1e3f3612e2e19f8
Alexei Shevtsov <a.shevcov@idaproject.com>
2023-11-07 00:10:13 +0300

    fix(users): Переименовал RateAPIView в RateViewSet. Ветка - feat_backend_Rating

--- backend/config/routers.py
+++ backend/config/routers.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from offers.views import AdvertisementModelViewSet
-from users.views import RateAPIView
+from users.views import RateViewSet
 
 router = DefaultRouter()
+
 router.register("advertisements", AdvertisementModelViewSet, basename="advertisements")
-router.register("user_rate", RateAPIView, basename="user_rate")
+router.register("user_rate", RateViewSet, basename="user_rate")
