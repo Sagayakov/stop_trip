8c32fed780d1f500896b9ca99a7c1f3a1b932d88
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-08 16:51:16 +0100

    feat: add photo listing

--- frontend/src/features/advert/Advert.tsx
+++ frontend/src/features/advert/Advert.tsx
@@ -2,10 +2,20 @@ import { useState } from 'react';
 import { ProductType } from '../../pages/advertPage/libr/types';
 import { BreadCrumbs } from '../../widgets/breadCrumbs/BreadCrumbs';
 import './libr/advert.scss';
+import { ArrowRight } from '../../shared/ui/icons/icons-tools/ArrowRight';
+import { ArrowLeft10x24 } from '../../shared/ui/icons/icons-tools/ArrowLeft10x24';
 
 export const Advert = ({ data }: { data: ProductType }) => {
     console.log(data);
     const [activeImage, setActiveImage] = useState(0);
+
+    const handleClickPrev = () => {
+        activeImage > 0 && setActiveImage(activeImage - 1);
+    };
+
+    const handleClickNext = () => {
+        activeImage < data.images.length - 1 && setActiveImage(activeImage + 1);
+    };
 
     return (
         <div className="advert-wrapper">
@@ -20,11 +30,16 @@ export const Advert = ({ data }: { data: ProductType }) => {
                 <section className="product-info">
                     <div className="image-wrapper">
                         <div className="active-image">
+                            <ArrowLeft10x24 handleClickPrev={handleClickPrev} />
                             <img
                                 src={
                                     //data.images[activeImage].image ||
                                     '../../../src/entities/lastAdverts/ui/image-not-found.jpg'
                                 }
+                            />
+                            <ArrowRight
+                                color="black"
+                                handleClickNext={handleClickNext}
                             />
                         </div>
                         <div className="image-list">
--- frontend/src/shared/ui/icons/icons-tools/ArrowLeft10x24.tsx
+++ frontend/src/shared/ui/icons/icons-tools/ArrowLeft10x24.tsx
@@ -1,13 +1,14 @@
 interface Props {
     color?: string;
     style?: Style;
+    handleClickPrev: () => void;
 }
 interface Style {
     marginRight: string;
     cursor: string;
 }
 
-export const ArrowLeft10x24 = ({ style }: Props) => {
+export const ArrowLeft10x24 = ({ style, handleClickPrev }: Props) => {
     return (
         <>
             <svg
@@ -17,6 +18,7 @@ export const ArrowLeft10x24 = ({ style }: Props) => {
                 viewBox="0 0 10 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                onClick={handleClickPrev}
             >
                 <path
                     d="M8 17L3.72196 12.8931C3.32333 12.5104 3.31065 11.8769 3.69366 11.4786L8 7"
