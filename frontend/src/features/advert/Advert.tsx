a66894d314d8eac435b19ab1f36ac5a505a5292b
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-09 14:11:46 +0100

    refactor: move characteristics to separate component

--- frontend/src/features/advert/Advert.tsx
+++ frontend/src/features/advert/Advert.tsx
@@ -3,6 +3,7 @@ import './libr/advert.scss';
 import { PhotoSlider } from '../../entities/photoSlider/PhotoSlider';
 import { useGetAdvertByIdQuery } from '../../app/api/fetchAdverts';
 import { useParams } from 'react-router-dom';
+import { AdvertCharacteristics } from '../../entities/advertCharacteristics/AdvertCharacterictics';
 
 export const Advert = () => {
     const { id } = useParams();
@@ -25,68 +26,7 @@ export const Advert = () => {
                     <div className="advert-info">
                         <section className="product-info">
                             <PhotoSlider />
-                            <div className="description">
-                                <h2 className="characteristics-header">
-                                    Характеристики
-                                </h2>
-                                <div className="characteristics-wrapper">
-                                    <span className="characteristics-key">
-                                        <p>
-                                            Общая площадь
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Жилая площадь
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Площадь кухни
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Количество комнат
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Санузел
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Балкон
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Ремонт
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Этаж
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Окна выходят
-                                            <span className="dashes"></span>
-                                        </p>
-                                        <p>
-                                            Материал стен
-                                            <span className="dashes"></span>
-                                        </p>
-                                    </span>
-                                    <span className="characteristics-value">
-                                        <p></p>
-                                        <p>46</p>
-                                        <p>20</p>
-                                        <p>8</p>
-                                        <p>2</p>
-                                        <p>Раздельный</p>
-                                        <p>Лоджия</p>
-                                        <p>Косметический</p>
-                                        <p>2</p>
-                                        <p>Во двор</p>
-                                        <p>Кирпич</p>
-                                    </span>
-                                </div>
-                            </div>
+                            <AdvertCharacteristics />
                         </section>
                         <section className="owner-info"></section>
                     </div>
