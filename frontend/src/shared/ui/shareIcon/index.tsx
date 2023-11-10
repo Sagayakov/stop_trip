d32471d69121c4469ee5d9289e58457104f825ed
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-10 22:46:13 +0100

    feat: add listener to share icon

--- frontend/src/shared/ui/shareIcon/index.tsx
+++ frontend/src/shared/ui/shareIcon/index.tsx
@@ -1,9 +1,20 @@
 import { Share } from '../icons/icons-tools/Share';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ShareIcon = () => {
+    const handleClick = () => {
+        const path = document.location.href;
+        console.log(path);
+        toast.info('Скопировано!');
+    };
+
     return (
-        <div className="share-icon">
-            <Share />
+        <div className="share-block">
+            <div className="share-icon" onClick={handleClick}>
+                <Share />
+            </div>
+            <ToastContainer />
         </div>
     );
 };
