d46c57f08b55ab6f21aaa93729cd776c88768447
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-14 15:06:47 +0100

    fix: fix rating

--- frontend/src/shared/ui/Rating/index.tsx
+++ frontend/src/shared/ui/Rating/index.tsx
@@ -1,32 +1,37 @@
 import { useState } from 'react';
-//import { HalfStar } from '../icons/icons-tools/HalfStar';
 import { Star } from '../icons/icons-tools/Star';
+import { toast } from 'react-toastify';
 
 type RatingProps = {
     rating: number;
+    grades: number;
+    setGrades: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const Rating = ({ rating }: RatingProps) => {
-    const [activeStar, setActiveStar] = useState(5);
+export const Rating = ({ rating, grades, setGrades }: RatingProps) => {
+    const [activeStar, setActiveStar] = useState(0);
+    const [prepareStar, setPrepareStar] = useState(0);
 
     const starsAmount = 5;
     const starArray = new Array(starsAmount).fill(1).map((el, i) => el + i);
 
+    const handleClick = () => {
+        setGrades(grades + 1);
+        toast.success('Спасибо! Ваша оценка добавлена!');
+    };
+
     return (
-        <p aria-label={`Rating is ${rating} out of ${starsAmount}`}>
+        <p
+            aria-label={`Rating is ${rating} out of ${starsAmount}`}
+            onClick={handleClick}
+        >
             {starArray.map((item, i) => {
-                /* rating < item ? (
-                    <HalfStar 
-                        key={item}
-                        id={i + 1}
-                        activeStar={activeStar}
-                        setActiveStar={setActiveStar} //нужно будет при необходимости отрисовки частичной звезды
-                    />
-                ) : ( */
                 return (
                     <Star
                         key={item}
                         id={i + 1}
+                        prepareStar={prepareStar}
+                        setPrepareStar={setPrepareStar}
                         activeStar={activeStar}
                         setActiveStar={setActiveStar}
                     />
