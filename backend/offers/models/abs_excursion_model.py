bb62ff5414149f31a633dda2402b2bb886c70a01
Sagayakov <sadayfrom@gmail.com>
2023-11-20 12:44:19 +0500

    feat(offers): правки

--- backend/offers/models/abs_excursion_model.py
+++ backend/offers/models/abs_excursion_model.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class AbsExcursion(models.Model):
+    """Абстрактная модель экскурсий"""
+
     excursion_food = models.BooleanField(verbose_name="Включена еда", default=False)
     excursion_transfer = models.BooleanField(verbose_name="Трансфер", default=False)
 
