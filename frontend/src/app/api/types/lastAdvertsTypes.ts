2d80d299e63f56d780ff44f2139ccfed4af6675e
Semyon Konevskiy <konevskiy7on@gmail.com>
2023-11-21 10:58:51 +0300

    feat: added output of the date of advert creation on main page

--- frontend/src/app/api/types/lastAdvertsTypes.ts
+++ frontend/src/app/api/types/lastAdvertsTypes.ts
@@ -5,6 +5,7 @@ export interface LastAdvertsTypes {
     price: number;
     description?: string;
     images: LastAdvertsImages[];
+    date_create: string
 }
 
 export interface LastAdvertsImages {
