4736af4e4765c0fb0ec91dbd42521ed752113ba6
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-25 21:07:37 +0100

    fix: fix portal photo

--- frontend/src/entities/portal/Portal.tsx
+++ frontend/src/entities/portal/Portal.tsx
@@ -1,9 +1,8 @@
-import { ArrowLeft10x24 } from '../../shared/ui/icons/icons-tools/ArrowLeft10x24';
 import { useMatchMedia } from '../../app/hooks/useMatchMedia';
 import './portal.scss';
-import { ArrowRight } from '../../shared/ui/icons/icons-tools/ArrowRight';
 import { LastAdvertsImages } from '../../app/api/types/lastAdvertsTypes';
 import { Close } from '../../shared/ui/icons/icons-tools/Close';
+import { useState } from 'react';
 
 type PortalProps = {
     image: string;
@@ -23,24 +22,51 @@ export const Portal = ({
     handleClickPortalNext,
 }: PortalProps) => {
     const { isTablet, isMobile } = useMatchMedia();
+    const [touchStart, setTouchStart] = useState<{identifier: number, screenX: number} | null>(null);
 
     const handleClick = () => {
         setIsPortalOpen(false);
     };
+
+    const handleTouchStart = (event: React.TouchEvent<HTMLImageElement>) => {
+        if (isMobile) {
+            setTouchStart({
+                identifier: event.changedTouches[0].identifier,
+                screenX: event.changedTouches[0].screenX,
+            });
+        } else {
+            return;
+        }
+    };
+
+    const handleTouchEnd = (event: React.TouchEvent<HTMLImageElement>) => {
+        if (isMobile) {
+            if ( touchStart?.identifier === event.changedTouches[0].identifier) {
+                if (event.changedTouches[0].screenX < touchStart.screenX) {
+                    handleClickPortalNext();
+                }
+                if (event.changedTouches[0].screenX > touchStart.screenX) {
+                    handleClickPortalPrev();
+                }
+                return;
+            }
+        } else {
+            return;
+        }
+    }
     
     return (
         <div className="portal">
             {isTablet || isMobile ? (
                 <>
-                    <img src={image} alt="Photo" />
+                    <img
+                        src={image}
+                        alt="Photo"
+                        onTouchStart={handleTouchStart}
+                        onTouchEnd={handleTouchEnd}
+                    />
                     <div className="close-portal" onClick={handleClick}>
                         <Close color='white' />
-                    </div>
-                    <div className="prev">
-                        <ArrowLeft10x24 color='white' handleClickPrev={handleClickPortalPrev} />
-                    </div>
-                    <div className="next">
-                        <ArrowRight color='white' handleClickNext={handleClickPortalNext} />
                     </div>
                     <div className="pages" onClick={handleClick}>
                         <span>{`${active}/`}</span>
