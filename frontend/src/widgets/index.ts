447f1ef8ecdca80833e14adad6c9ee47fe90b4af
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-11-28 20:01:36 +0100

    feat: add template for currency filters

--- frontend/src/widgets/index.ts
+++ frontend/src/widgets/index.ts
@@ -2,8 +2,9 @@ export { AnyCategory } from './anyCategory/AnyCategory';
 export { Footer } from './footer/Footer';
 export { Header } from './header/Header';
 export { SettingRealtyForm } from './settingForm/settingRealty/SettingRealtyForm';
-export { SettingTransportForm } from './settingForm/settingTransport/SettingTransportForm'
-export { SettingTaxiForm } from './settingForm/settingTaxi/SettingTaxiForm'
-export { SettingServicesForm } from './settingForm/settingServices/SettingServicesForm'
-export { SettingJobForm } from './settingForm/settingJob/SettingJobFilter'
-export { SettingEventForm } from './settingForm/settingEvent/SettingEventForm'
+export { SettingTransportForm } from './settingForm/settingTransport/SettingTransportForm';
+export { SettingTaxiForm } from './settingForm/settingTaxi/SettingTaxiForm';
+export { SettingServicesForm } from './settingForm/settingServices/SettingServicesForm';
+export { SettingJobForm } from './settingForm/settingJob/SettingJobFilter';
+export { SettingEventForm } from './settingForm/settingEvent/SettingEventForm';
+export { SettingCurrencyForm } from './settingForm/settingCurrency/SettingCurrencyForm';
---
+++ frontend/src/widgets/settingForm/settingCurrency/SettingCurrencyForm.tsx
@@ -0,0 +1,38 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Reset } from '../../../shared/ui/icons/icons-tools/Reset';
+import { TypeOfCurrencyFilter } from './libr/TypeOfCurrencyFilter';
+import './libr/settingCurrencyFilter.scss';
+
+interface Props {
+    setShowFilters: (value: React.SetStateAction<boolean>) => void;
+}
+
+export const SettingCurrencyForm = ({ setShowFilters }: Props) => {
+    const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        event.stopPropagation();
+    };
+
+    const { register, handleSubmit, reset } =
+        useForm<TypeOfCurrencyFilter>();
+
+    const onSubmit: SubmitHandler<TypeOfCurrencyFilter> = (data) => {
+        console.log(data);
+        setShowFilters(false);
+        reset();
+    };
+
+    const onReset = () => reset();
+
+    return (
+        <section className="filters" onClick={handleClick}>
+            <form className="filter-currency-form" onSubmit={handleSubmit(onSubmit)}>
+                
+                <input type="submit" value="Показать 100 объявлений" />
+                <button className="reset-setting-form" onClick={onReset}>
+                    <Reset color="#1F6FDE" />
+                    Сбросить фильтры
+                </button>
+            </form>
+        </section>
+    );
+};
