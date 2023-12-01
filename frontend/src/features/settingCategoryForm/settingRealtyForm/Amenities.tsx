52917d0cf593f574579a16fda9bfd71d8bc67dc3
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-12-01 18:30:10 +0100

    fix: rewrite amenities filter

--- frontend/src/features/settingCategoryForm/settingRealtyForm/Amenities.tsx
+++ frontend/src/features/settingCategoryForm/settingRealtyForm/Amenities.tsx
@@ -1,4 +1,34 @@
 import { UseFormRegister } from 'react-hook-form';
+import { valuesOfPropertyForm } from '../../../widgets/settingForm/settingRealty/libr/valuesOfPropertyForm';
+import { TypeSettingRealty } from '../../../widgets/settingForm/settingRealty/libr/TypeSettingRealty';
+
+interface Props {
+    register: UseFormRegister<TypeSettingRealty>;
+}
+
+export const Amenities = ({ register }: Props) => {
+    const amenitiesTypeValues = valuesOfPropertyForm.property_amenities;
+
+    return (
+        <div className="amenities">
+            <h3>Удобства</h3>
+            <div className="amenities-setting">
+                {amenitiesTypeValues.map((el) => (
+                    <label className="form-checkbox" key={el.label}>
+                        <input
+                            type="checkbox"
+                            value={el.value}
+                            {...register('property_amenities')}
+                        />
+                        <span>{el.label}</span>
+                    </label>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+/* import { UseFormRegister } from 'react-hook-form';
 import { TypeSettingRealty } from '../../../widgets/settingForm/settingRealty/libr/TypeSettingRealty';
 
 interface Props {
@@ -17,4 +47,4 @@ export const Amenities = ({ register }: Props) => {
             </div>
         </div>
     );
-};
+}; */
