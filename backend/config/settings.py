ff05be01846bba02744cff402077e545cdc7fa51
Sagayakov <sadayfrom@gmail.com>
2023-12-04 23:40:20 +0500

    feat(offers): fix JWT

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -246,7 +246,10 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
 )
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
+    "AUTH_HEADER_TYPES": (
+        "Bearer",
+        "JWT",
+    ),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
