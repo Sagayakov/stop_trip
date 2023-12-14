6c566f0d7dded21396674231cf102c89982b789e
Aziza <azizamukashkyzy@gmail.com>
2023-12-14 18:13:53 +0600

    fix(settings)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -237,7 +237,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
