7cd74abd3a2a24236a20b772adf6b69fe4bfb19f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-12-15 15:50:15 +0100

    feat: add translation to modals

--- frontend/src/features/header/modal/modalCheckEmail/ModalCheckEmail.tsx
+++ frontend/src/features/header/modal/modalCheckEmail/ModalCheckEmail.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Close } from '../../../../shared/ui/icons/icons-tools/Close';
 import './modalCheckEmail.scss';
+import { useTranslation } from 'react-i18next';
 
 export const ModalCheckEmail = () => {
     const [isOpen, setIsOpen] = useState(true);
+    const { t } = useTranslation();
 
     return (
         <div
@@ -12,8 +14,7 @@ export const ModalCheckEmail = () => {
         >
             <div className={isOpen ? 'modal-check-content' : 'close'}>
                 <Close onclick={() => setIsOpen(false)} />
-                Чтобы завершить процесс регистрации, перейдите по ссылке,
-                отправленной на указанный адрес электронной почты
+                {t('modal-check-email')}
             </div>
         </div>
     );
