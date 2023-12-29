d55cc0202f296dea1c4f78ad5794a8598b7f4864
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-12-29 20:15:17 +0100

    feat: prettify event date on advert page

--- frontend/src/entities/advert/advertCharacteristics/utils/getValuesList.tsx
+++ frontend/src/entities/advert/advertCharacteristics/utils/getValuesList.tsx
@@ -5,6 +5,8 @@ import {
     categoryCharacteristicsValues,
 } from 'shared/const/categoryCharacteristics.ts';
 import { useTranslation } from 'react-i18next';
+import { getEventDate } from 'shared/utils/getEventDate';
+import styles from '../advertCharacteristics.module.scss';
 
 type GetListProps = {
     data: ProductType;
@@ -23,7 +25,7 @@ export const GetValuesList = ({ data, category }: GetListProps) => {
             key === 'is_published' ||
             data[key] === '' ||
             key === 'owner' ||
-            key === 'property_city'
+            key === 'city'
         ) {
             continue;
         } else if (key === 'property_amenities') {
@@ -50,9 +52,11 @@ export const GetValuesList = ({ data, category }: GetListProps) => {
                 </div>
             );
         } else if (key === 'start_date' || key === 'end_date') {
+            const date = getEventDate(data[key] as string);
             list.push(
-                <div key={key}>
-                    <p>{data[key]}</p>
+                <div key={key} className={styles.event_date}>
+                    <span>{data[key] && date.day}</span>
+                    <span>{data[key] && date.time}</span>
                 </div>
             );
         } else {
