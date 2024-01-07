0349fe5e33e64fae227493bb1ef67633a7f8b69a
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-07 17:25:24 +0100

    feat: improve pagination

--- frontend/src/features/pagination/libr/beginningParentScroll.ts
+++ frontend/src/features/pagination/libr/beginningParentScroll.ts
@@ -1,8 +1,8 @@
-export const beginnigParentScroll = (ref: React.RefObject<HTMLElement>) => {
+export const beginningParentScroll = (ref: React.RefObject<HTMLElement>) => {
     const parent = ref.current?.parentElement;
     if (parent) {
         const parentRect = parent.getBoundingClientRect();
         const parentOffsetTop = parentRect.top + window.scrollY - 50;
         window.scrollTo({ top: parentOffsetTop, behavior: 'smooth' });
     }
-}
+};
--- frontend/src/pages/categoryPage/CategoryPage.tsx
+++ frontend/src/pages/categoryPage/CategoryPage.tsx
@@ -1,7 +1,9 @@
-import { lazy, Suspense, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { lazy, Suspense, useRef, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useMatchMedia } from 'app/hooks/useMatchMedia.ts';
-export const AnyCategory = lazy(() => import('widgets/anyCategory/AnyCategory'));
+export const AnyCategory = lazy(
+    () => import('widgets/anyCategory/AnyCategory')
+);
 export const Controls = lazy(() => import('features/controls/Controls'));
 import { ArrowLeft10x24 } from 'shared/ui/icons/icons-tools/ArrowLeft10x24.tsx';
 import { HorizontalMixer } from 'shared/ui/icons/icons-tools/HorizontalMixer.tsx';
@@ -9,12 +11,17 @@ import { LoadingWithBackground } from 'entities/loading/LoadingWithBackground.ts
 import { useTranslation } from 'react-i18next';
 import { FilterForms } from 'widgets/settingForm/forms/FilterForms.tsx';
 import style from './style/categoryPage.module.scss';
+import { useGetAdvertsQuery } from 'app/api/fetchAdverts';
+import { Pagination } from 'features/pagination';
 
 const CategoryPage = () => {
     const category = location.pathname.split('/')[1];
     const [showFilters, setShowFilters] = useState<boolean>(false);
     const { isDesktop } = useMatchMedia();
     const { t } = useTranslation();
+    const queryParam = useLocation().search;
+    const { data } = useGetAdvertsQuery(queryParam);
+    const parentRef = useRef<HTMLDivElement>(null);
 
     const filterBtnStyle = {
         display: `${isDesktop ? 'none' : 'flex'}`,
@@ -55,11 +62,17 @@ const CategoryPage = () => {
                     </div>
                 </div>
             )}
-            <div className={style.filters_announcement}>
-                <Suspense fallback={<LoadingWithBackground />}>
-                        <FilterForms showFilters={showFilters} setShowFilters={setShowFilters} />
-                    <AnyCategory />
-                </Suspense>
+            <div className={style.category_content_wrapper} ref={parentRef}>
+                <div className={style.filters_announcement}>
+                    <Suspense fallback={<LoadingWithBackground />}>
+                        <FilterForms
+                            showFilters={showFilters}
+                            setShowFilters={setShowFilters}
+                        />
+                        <AnyCategory />
+                    </Suspense>
+                </div>
+                <Pagination data={data} parentRef={parentRef} />
             </div>
         </>
     );
