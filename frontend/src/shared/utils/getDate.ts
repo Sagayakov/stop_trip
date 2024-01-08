b731887af96970c84bbb596e225adcc55aab8502
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-08 18:15:19 +0100

    feat: prettify public date

--- frontend/src/shared/utils/getDate.ts
+++ frontend/src/shared/utils/getDate.ts
@@ -13,7 +13,9 @@ export const getDate = (publicDate: string) => {
         todayYear: today.getFullYear(),
     };
 
-    let dayToDisplay: string = `${day}.${month + 1}.${year}`;
+    let dayToDisplay: string = `${day < 10 ? `0${day}` : day}.${
+        month < 9 ? `0${month + 1}` : month + 1
+    }.${year}`;
 
     if (month === todayMonth && year === todayYear) {
         if (day === todayDay) {
