13833867a6847fd50393fc94a0a04949a04f0ce9
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-08 14:45:17 +0100

    fix: remove 24 hours in pric block

--- frontend/src/entities/advert/priceBlock/PriceBlock.tsx
+++ frontend/src/entities/advert/priceBlock/PriceBlock.tsx
@@ -1,7 +1,6 @@
 import styles from 'widgets/advert/libr/advert.module.scss';
 import { ProductType } from 'pages/advertPage/libr/types.ts';
 import { useTranslation } from 'react-i18next';
-import { useMatchMedia } from 'app/hooks/useMatchMedia.ts';
 import { prettifyPrice } from 'shared/utils/prettifyPrice';
 
 interface Props {
@@ -9,20 +8,14 @@ interface Props {
 }
 export const PriceBlock = ({ data }: Props) => {
     const { t } = useTranslation();
-    const { isMobile } = useMatchMedia();
 
     return (
         <>
             <div className={styles.price_block}>
-                {t('advert-page.day')}
                 <span className={styles.price}>
                     {data.price
                         ? prettifyPrice(data.price)
-                        : `${t(
-                              `advert-page.${
-                                  !isMobile ? 'negotiated' : 'price-negotiated'
-                              }`
-                          )}`}
+                        : `${t('advert-page.price-negotiated')}`}
                 </span>
             </div>
         </>
