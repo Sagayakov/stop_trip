991e1fcbcbe98c3561e5419b593d2d7f3c2cc780
Alexei Shevtsov <a.shevcov@idaproject.com>
2024-01-08 17:27:06 +0300

    fix(offers): Поправил ручку get_available_filtered_params

--- backend/offers/filters/currency_exchange_filter.py
+++ backend/offers/filters/currency_exchange_filter.py
@@ -50,17 +50,17 @@ class CurrencyExchange(FilterSet):
         # Предлагаемая валюта
         facets["proposed_currency"] = (
             queryset.exclude(proposed_currency__isnull=True)
-            .values_list("proposed_currency", flat=True)
-            .order_by("proposed_currency")
-            .distinct("proposed_currency")
+            .values_list("proposed_currency__short_name", flat=True)
+            .order_by("proposed_currency__short_name")
+            .distinct("proposed_currency__short_name")
         )
 
         # Обмен на
         facets["exchange_for"] = (
             queryset.exclude(exchange_for__isnull=True)
-            .values_list("exchange_for", flat=True)
-            .order_by("exchange_for")
-            .distinct("exchange_for")
+            .values_list("exchange_for__short_name", flat=True)
+            .order_by("exchange_for__short_name")
+            .distinct("exchange_for__short_name")
         )
 
         return facets
