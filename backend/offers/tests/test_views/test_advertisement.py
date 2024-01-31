864286a758d424d6cf7686989ff8ea422cf206ca
Sagayakov <sadayfrom@gmail.com>
2024-01-31 23:03:26 +0500

    fix(back): add tests

--- backend/offers/tests/test_views/test_advertisement.py
+++ backend/offers/tests/test_views/test_advertisement.py
@@ -31,7 +31,7 @@ from offers.constants import (
     MarketCondition,
     PropertyType,
 )
-from users.tests.factories import UserFactory
+from users.tests.factories import UserFactory, UserMessengerFactory
 from ..factories import BaseAdvertisementFactory, CountryFactory, RegionFactory, CityFactory
 from offers.models import Advertisement
 
@@ -90,6 +90,7 @@ class AdvertisementViewSetTest(APITestCase):
 
     def test_detail(self):
         user = UserFactory()
+        user_messenger = UserMessengerFactory(owner=user)
         advertisements = [
             BaseAdvertisementFactory(owner=user, category=category)
             for category in CategoryChoices.values
@@ -103,6 +104,17 @@ class AdvertisementViewSetTest(APITestCase):
         self.assertEqual(res_json["slug"], advertisements[0].slug)
         self.assertTrue(res_json["owner"])
         self.assertEqual(res_json["owner"]["id"], user.id)
+        self.assertEqual(
+            res_json["owner"]["user_messengers"][0]["messenger"]["id"], user_messenger.id
+        )
+        self.assertEqual(
+            res_json["owner"]["user_messengers"][0]["link_to_user"],
+            user_messenger.link_to_user,
+        )
+        self.assertEqual(
+            res_json["owner"]["user_messengers"][0]["messenger"]["name"],
+            user_messenger.messenger.name,
+        )
         self.assertIsNotNone(res_json["owner"]["avg_rating"])
         self.assertIsNotNone(res_json["owner"]["rating_num"])
         self.assertIsNone(res_json["owner"]["my_rating"])
--- backend/users/views/messenger_viewsets.py
+++ backend/users/views/messenger_viewsets.py
@@ -1,18 +1,29 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    UpdateModelMixin,
+    ListModelMixin,
+)
 from rest_framework.viewsets import GenericViewSet
 
 from offers.permissions import OwnerOrAdminPermission
 from ..models import UserMessenger
-from ..serializers import MessengerCreateSerializer
+from ..serializers import MessengerCreateSerializer, MessengerListSerializer
 
 
 @extend_schema(tags=["Messenger"])
-class MessengerViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+class MessengerViewSet(
+    CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet, ListModelMixin
+):
     """Мессенджеры."""
 
-    serializer_class = MessengerCreateSerializer
     permission_classes = [OwnerOrAdminPermission]
+
+    def get_serializer_class(self):
+        if self.action in [self.create.__name__, self.update.__name__]:
+            return MessengerCreateSerializer
+        return MessengerListSerializer
 
     def get_queryset(self):
         return UserMessenger.objects.filter(owner=self.request.user)
