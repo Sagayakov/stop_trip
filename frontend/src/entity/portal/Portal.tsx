cfce4f759f55122b8a86918d6048fc71b78dd209
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-19 15:09:01 +0100

    feat: add video to product page

--- frontend/src/entity/portal/Portal.tsx
+++ frontend/src/entity/portal/Portal.tsx
@@ -5,14 +5,17 @@ import { Close } from 'shared/ui/icons/icons-tools/Close.tsx';
 import { useEffect, useRef, useState } from 'react';
 import { ArrowLeft10x24 } from 'shared/ui/icons/icons-tools/ArrowLeft10x24.tsx';
 import { ArrowRight } from 'shared/ui/icons/icons-tools/ArrowRight.tsx';
+import { YoutubeEmbed } from 'features/youtubeEmbed';
 
 type PortalProps = {
-    image: string;
+    image: string | undefined;
     setIsPortalOpen: React.Dispatch<React.SetStateAction<boolean>>;
     images: LastAdvertsImages[];
     active: number;
     handleClickPortalPrev: () => void;
     handleClickPortalNext: () => void;
+    isVideo: boolean;
+    link: string | null;
 };
 
 export const Portal = ({
@@ -22,9 +25,14 @@ export const Portal = ({
     active,
     handleClickPortalPrev,
     handleClickPortalNext,
+    isVideo,
+    link,
 }: PortalProps) => {
     const { isMobile } = useMatchMedia();
-    const [touchStart, setTouchStart] = useState<{identifier: number, screenX: number} | null>(null);
+    const [touchStart, setTouchStart] = useState<{
+        identifier: number;
+        screenX: number;
+    } | null>(null);
     const [imageWidth, setImageWidth] = useState<number>(0);
     const [imageHeight, setImageHeight] = useState<number>(0);
     const imageRef = useRef<null | HTMLImageElement>(null);
@@ -32,6 +40,7 @@ export const Portal = ({
     const handleClick = () => {
         setIsPortalOpen(false);
     };
+
     const handleKeyDown = (event: KeyboardEvent) => {
         if (event.key === 'Escape') {
             setIsPortalOpen(false);
@@ -55,7 +64,7 @@ export const Portal = ({
 
     const handleTouchEnd = (event: React.TouchEvent<HTMLImageElement>) => {
         if (isMobile) {
-            if ( touchStart?.identifier === event.changedTouches[0].identifier) {
+            if (touchStart?.identifier === event.changedTouches[0].identifier) {
                 if (event.changedTouches[0].screenX < touchStart.screenX) {
                     handleClickPortalNext();
                 }
@@ -65,7 +74,7 @@ export const Portal = ({
             }
         }
         return;
-    }
+    };
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeyDown);
@@ -73,22 +82,27 @@ export const Portal = ({
             document.removeEventListener('keydown', handleKeyDown);
         };
     }, []);
-    
+
     return (
         <div className={styles.portal}>
-            <img
-                src={image}
-                alt="Photo"
-                ref={imageRef}
-                onLoad={handleOnLoad}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-                className={
-                    imageWidth > imageHeight
-                        ? `${styles.horizontal}`
-                        : `${styles.vertical}`
-                }
-            />
+            {image && active <= images.length ? (
+                <img
+                    src={image}
+                    alt="Photo"
+                    ref={imageRef}
+                    onLoad={handleOnLoad}
+                    onTouchStart={handleTouchStart}
+                    onTouchEnd={handleTouchEnd}
+                    className={
+                        imageWidth > imageHeight
+                            ? `${styles.horizontal}`
+                            : `${styles.vertical}`
+                    }
+                />
+            ) : (
+                <YoutubeEmbed link={link!} />
+            )}
+
             <div className={styles.close_portal} onClick={handleClick}>
                 <Close color="white" />
             </div>
@@ -106,7 +120,7 @@ export const Portal = ({
             </div>
             <div className={styles.pages} onClick={handleClick}>
                 <span>{`${active}/`}</span>
-                <span>{`${images.length}`}</span>
+                <span>{`${isVideo ? images.length + 1 : images.length}`}</span>
             </div>
         </div>
     );
