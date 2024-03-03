25df0385ddc8b5a33366fefc9dab97a758604cd1
Semyon Konevskiy <konevskiy7on@gmail.com>
2024-03-03 16:54:00 +0400

    fix: переделал на конвертацию в base64

--- frontend/src/pages/addAnnouncement/libr/convertFileToBinary.ts
+++ frontend/src/pages/addAnnouncement/libr/convertFileToBinary.ts
@@ -1,25 +1,55 @@
-const fileToBinaryString = (file: File) => {
+// const fileToBinaryString = (file: File) => {
+//     return new Promise((resolve, reject) => {
+//         const reader = new FileReader();
+//         reader.onload = function(event) {
+//             resolve(event.target?.result);
+//         };
+//         reader.onerror = function(error) {
+//             reject(error);
+//         };
+//         reader.readAsBinaryString(file);
+//     });
+// }
+// export const convertFilesToBinaryStrings = async (files: File[]) => {
+//     const binaryStrings = [];
+//     for (const file of files) {
+//         try {
+//             const binaryString = await fileToBinaryString(file);
+//             binaryStrings.push(binaryString);
+//         } catch (error) {
+//             console.error('Ошибка при чтении файла:', error);
+//         }
+//     }
+//     return binaryStrings;
+// }
+const fileToBase64String = (file: File) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onload = function(event) {
-            resolve(event.target?.result);
+            if (event.target?.result) {
+                const result = event.target.result as string;
+                const base64String = result.split(',')[1];
+                resolve(base64String);
+            } else {
+                reject(new Error("Ошибка чтения файла"));
+            }
         };
         reader.onerror = function(error) {
             reject(error);
         };
-        reader.readAsBinaryString(file);
+        reader.readAsDataURL(file);
     });
-}
+};
 
-export const convertFilesToBinaryStrings = async (files: File[]) => {
-    const binaryStrings = [];
+export const convertFilesToBase64Strings = async (files: File[]) => {
+    const base64Strings: string[] = [];
     for (const file of files) {
         try {
-            const binaryString = await fileToBinaryString(file);
-            binaryStrings.push(binaryString);
+            const base64String = await fileToBase64String(file);
+            base64Strings.push(base64String as string);
         } catch (error) {
             console.error('Ошибка при чтении файла:', error);
         }
     }
-    return binaryStrings;
-}
+    return base64Strings;
+};
