8f6d67f4198567989d56538529677026bd50a3b5
Semyon Konevskiy <konevskiy7on@gmail.com>
2024-03-05 19:13:14 +0400

    fix: начал рефакторить загрузку картинок, объявления добавляются

--- frontend/src/features/addAnnouncementForm/universalFields/annPhoto/AnnouncementPhotoField.tsx
+++ frontend/src/features/addAnnouncementForm/universalFields/annPhoto/AnnouncementPhotoField.tsx
@@ -1,19 +1,16 @@
 import { SetStateAction, useEffect, useRef, useState } from 'react';
-import { ErrorOption, FieldPath, UseFormSetValue } from 'react-hook-form';
+import { ErrorOption, FieldPath, UseFormSetValue, UseFormWatch } from 'react-hook-form';
 import { FormAddAnn } from 'pages/addAnnouncement/libr/AnnouncementFormTypes.ts';
 import { MiniLoadPhoto } from 'shared/ui/icons/loadPhoto';
 import { useTranslation } from 'react-i18next';
 import styles from './annPhoto.module.scss';
 import { LoadPhotoBtn } from 'features/addAnnouncementForm/universalFields/annPhoto/annPhotoField/LoadPhotoBtn.tsx';
 import { LastAdvertsImages } from 'app/api/types/lastAdvertsTypes.ts';
-import { useLocation } from 'react-router-dom';
 import { toFixed } from 'ol/math';
-import { convertFilesToBase64Strings } from 'pages/addAnnouncement/libr/convertFileToBinary.ts';
 
 
 interface Props {
-    selectedImages: File[] | undefined;
-    setSelectedImages: React.Dispatch<React.SetStateAction<File[] | undefined>>;
+    watch: UseFormWatch<FormAddAnn>;
     setValue: UseFormSetValue<FormAddAnn>;
     editImages?: LastAdvertsImages[] | undefined;
     setEditImages?: React.Dispatch<React.SetStateAction<LastAdvertsImages[] | undefined>>
@@ -24,8 +21,7 @@ interface Props {
 }
 
 const AnnouncementPhotoField = ({
-    selectedImages,
-    setSelectedImages,
+    watch,
     setValue,
     editImages,
     setEditImages,
@@ -38,83 +34,54 @@ const AnnouncementPhotoField = ({
     const { t } = useTranslation();
     const [previewImages, setPreviewImages] = useState<string[]>([]);
     const [deleteIdArray, setDeleteIdArray] = useState<number[]>([]);
-    const path = useLocation().pathname.split('/');
+    const images = watch('images');
 
-    const removeImageUpload = (index: number) => {
-        if (selectedImages) {
-            const newImages = [...selectedImages];
-            newImages.splice(index, 1);
-            setSelectedImages(newImages);
+    const removeImage = (index: number) => {
+        if (images) {
+            const newImages = [...images];
+            newImages.splice(index, 1);//удаляем массив по индексу
+            setValue('images',newImages);
 
-            const newPreviews = [...previewImages];
+            const newPreviews = [...previewImages]
             newPreviews.splice(index, 1);
+
             setPreviewImages(newPreviews);
         }
     };
 
-    const removeImageEdid = (id: number) => {
-        if(editImages && editImages.length > 0){
-            setEditImages!((prevImages) => prevImages!.filter(img => img.id !== id));
-            setDeleteIdArray([...deleteIdArray, id]);
-        }
-    };
+    // const removeImageEdid = (id: number) => {
+    //     if(editImages && editImages.length > 0){
+    //         setEditImages!((prevImages) => prevImages!.filter(img => img.id !== id));
+    //         setDeleteIdArray([...deleteIdArray, id]);
+    //     }
+    // };
 
     const photoCounter = () => {
-        if(path[1] == 'advertisement-editing'){
-            if(editImages && selectedImages){
-                return selectedImages.length + editImages.length
-            } else if (editImages){
-                return editImages.length
-            } else if(selectedImages){
-                return selectedImages.length
-            } else return 0
-        } else{
-            return selectedImages?.length || 0
-        }
+        return previewImages?.length || 0
     }
 
     useEffect(() => {
-        if (selectedImages) {
-            if(path[1] === 'advertisement-editing'){
-                convertFilesToBase64Strings(selectedImages)
-                    .then((base64Strings) => {
-                        setValue('upload_images', base64Strings as string[])
-                    })
-                    .catch((error) => {
-                        console.error('Ошибка:', error);
-                    });
-            }else{
-                convertFilesToBase64Strings(selectedImages)
-                    .then((base64Strings) => {
-                        setValue('images', base64Strings as string[])
-                    })
-                    .catch((error) => {
-                        console.error('Ошибка:', error);
-                    });
-            }
-        }//если добаляем картинки, то их присваиваем полю upload_images
-        // если же находимся на странице добавления объявлений, то присваиваем полю
         if(editImages){
-            setValue('delete_images', deleteIdArray); //на бэк передаем массив id картинок, которые удаляем
-        }
-    }, [selectedImages, setValue, deleteIdArray]);
+            setValue('delete_images', deleteIdArray);
+        }//на бэк передаем массив id картинок, которые удаляем
+    }, [setValue, deleteIdArray]);
     
-    useEffect(() => {
-        if(selectedImages) {
-            const size = selectedImages.reduce((acc, img) => {
-                acc += img.size;
-                return acc;
-            }, 0)
-            if(size > 52428800) {
-                setError('upload_images', {});
-                setError('images', {});
-            } else{
-                clearErrors('images');
-                clearErrors('upload_images');
-            }
-            setImgSize(size);
-        }
-    }, [selectedImages]);
+    // useEffect(() => {
+    //     if(selectedImages) {
+    //         const size = selectedImages.reduce((acc, img) => {
+    //             acc += img.size;
+    //             return acc;
+    //         }, 0)
+    //         if(size > 52428800) {
+    //             setError('upload_images', {});
+    //             setError('images', {});
+    //         } else{
+    //             clearErrors('images');
+    //             clearErrors('upload_images');
+    //         }
+    //         setImgSize(size);
+    //     }
+    // }, [selectedImages]);
 
 
     return (
@@ -123,11 +90,12 @@ const AnnouncementPhotoField = ({
             <div className={styles.loadphoto}>
                 <div className={styles.loadphoto_btn_view}>
                     <LoadPhotoBtn
+                        setValue={setValue}
                         inputRef={inputRef}
-                        selectedImages={selectedImages}
-                        setSelectedImages={setSelectedImages}
                         setPreviewImages={setPreviewImages}
                         imgSize={imgSize}
+                        previewImages={previewImages}
+                        watch={watch}
                     />
                     <div className={styles.loadphoto_counter}>
                         <div className={styles.loadphoto_counter_wrapper}>
@@ -139,33 +107,33 @@ const AnnouncementPhotoField = ({
                         </div>
                     </div>
                 </div>
-                {((selectedImages && selectedImages.length > 0) ||
+                {((images && images.length > 0) ||
                     editImages) && (
                     //если при редактировании есть старые фотки, или если добавляем новые, то отрисовываем их
-                    <div className={styles.preview}>
-                        {editImages?.map((img) => (
-                            <div
-                                key={img.id}
-                                className={styles.btn_view_delete}
-                                onClick={() => removeImageEdid(img.id)}
-                            >
-                                <img
-                                    key={img.id}
-                                    src={img.image}
-                                    alt={`Preview ${img.id}`}
-                                    style={{
-                                        maxWidth: '100px',
-                                        margin: '5px',
-                                    }}
-                                />
-                                <span>&#x2716;</span>
-                            </div>
-                        ))}
+                    <div className={styles.preview}> {/*это редактируемые фотографии*/}
+                        {/*{editImages?.map((img) => (*/}
+                        {/*    <div*/}
+                        {/*        key={img.id}*/}
+                        {/*        className={styles.btn_view_delete}*/}
+                        {/*        onClick={() => removeImageEdid(img.id)}*/}
+                        {/*    >*/}
+                        {/*        <img*/}
+                        {/*            key={img.id}*/}
+                        {/*            src={img.image}*/}
+                        {/*            alt={`Preview ${img.id}`}*/}
+                        {/*            style={{*/}
+                        {/*                maxWidth: '100px',*/}
+                        {/*                margin: '5px',*/}
+                        {/*            }}*/}
+                        {/*        />*/}
+                        {/*        <span>&#x2716;</span>*/}
+                        {/*    </div>*/}
+                        {/*))}*/}
                         {previewImages?.map((preview, index) => (
                             <div
                                 key={index}
                                 className={styles.btn_view_delete}
-                                onClick={() => removeImageUpload(index)}
+                                onClick={() => removeImage(index)}
                             >
                                 <img
                                     key={index}
@@ -178,7 +146,7 @@ const AnnouncementPhotoField = ({
                                 />
                                 <span>&#x2716;</span>
                             </div>
-                        ))}
+                        ))}{/*это загружаемые фотографии*/}
                     </div>
                 )}
             </div>
