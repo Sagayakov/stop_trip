2495c992b0e795a1e7e9860ebab4dea39c0e1877
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-03-08 20:45:32 +0100

    feat: add user agreement link to footer

--- frontend/src/entity/footer/FooterBottomMobile.tsx
+++ frontend/src/entity/footer/FooterBottomMobile.tsx
@@ -2,8 +2,11 @@ import { Telegram } from 'shared/ui/icons/contacts/Telegram.tsx';
 import { WhatsApp } from 'shared/ui/icons/contacts/WhatsApp.tsx';
 import styles from 'widgets/footer/footer.module.scss';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const FooterBottomMobile = () => {
+    const { t } = useTranslation();
+
     return (
         <div className={styles.footer_bot}>
             <div className={styles.contacts}>
@@ -17,13 +20,22 @@ export const FooterBottomMobile = () => {
                     <WhatsApp color="#10bf3e" />
                 </Link>
             </div>
-            <a
-                href="mailto:admin@stoptrip.com"
-                target="_blank"
-                className={styles.email_link}
-            >
-                admin@stoptrip.com
-            </a>
+            <div className={styles.email_agreement}>
+                <a
+                    href="mailto:admin@stoptrip.com"
+                    target="_blank"
+                    className={styles.email_link}
+                >
+                    admin@stoptrip.com
+                </a>
+                <Link
+                    to="/user-agreement"
+                    target="_blank"
+                    className={styles.user_agreement}
+                >
+                    {t('user_agreement.link')}
+                </Link>
+            </div>
         </div>
     );
 };
