d4e24315e4353adea7b8d2ea8b9ccf4c962f3ab1
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-03-18 20:00:22 +0100

    fix: fix city input on edit page

--- frontend/src/features/addAnnouncementForm/universalFields/AnnouncementCity.tsx
+++ frontend/src/features/addAnnouncementForm/universalFields/AnnouncementCity.tsx
@@ -14,11 +14,12 @@ import {
 import { useEffect, useState } from 'react';
 import { useGetCitiesByRegionQuery } from 'app/api/fetchAdverts.ts';
 import { StringOptions } from 'app/api/types/selectOptionValues';
+import { NameType } from 'pages/advertPage/libr/types';
 
 interface Props {
     setValue: UseFormSetValue<FormAddAnn>;
     control: Control<FormAddAnn, string[]>;
-    defaultValue?: { name: string } | null | undefined;
+    defaultValue?: NameType | null | undefined;
     formState: FormState<FormAddAnn>;
     watch: UseFormWatch<FormAddAnn>;
 }
@@ -31,7 +32,6 @@ const AnnouncementCity = ({
     watch,
 }: Props) => {
     const { t } = useTranslation();
-    //const { data } = useGetSelectOptionsQuery('');
     const region = watch('region');
     const { data: availableData } = useGetCitiesByRegionQuery(
         `?region=${region || 'north-goa'}`
@@ -49,14 +49,14 @@ const AnnouncementCity = ({
             );
             if (defaultValue) {
                 const defCity = availableData.find(
-                    (el) => el.name === defaultValue.name
+                    (el) => el.slug === defaultValue.slug
                 );
                 setValue('city', defCity?.slug);
                 setDefaultCity({
                     value: defCity?.slug || '',
                     label: defCity?.name || '',
                 });
-            } //если есть значение по умолчанию, устанавливаем его. Если юзер поменяет выбор, то установится новое значение
+            }
         }
     }, [defaultValue, availableData]);
 
@@ -66,20 +66,18 @@ const AnnouncementCity = ({
                 {t('add-page.city')}
                 <span>*</span>:
             </h3>
-            {availableData && (
-                <UniversalSelectDropdown<FormAddAnn>
-                    setValue={setValue}
-                    control={control}
-                    name="city"
-                    prefix="filterAnnouncementCategory"
-                    placeholder={t('add-page.city')}
-                    closeMenuOnSelect={true}
-                    isMulti={false}
-                    options={options}
-                    requiredFiled={true}
-                    defaultValue={defaultCity}
-                />
-            )}
+            <UniversalSelectDropdown<FormAddAnn>
+                setValue={setValue}
+                control={control}
+                name="city"
+                prefix="filterAnnouncementCategory"
+                placeholder={t('add-page.city')}
+                closeMenuOnSelect={true}
+                isMulti={false}
+                options={options}
+                requiredFiled={true}
+                defaultValue={defaultCity}
+            />
             <div className={styles.ann_field_err}>
                 {formState?.errors?.city?.message}
             </div>
