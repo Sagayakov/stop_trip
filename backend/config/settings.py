ff39f4f9030c9023b51542d483da5d903912641b
Sagayakov <sadayfrom@gmail.com>
2024-03-24 13:35:01 +0500

    fix(back): fix mail

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -319,7 +319,7 @@ EMAIL_PORT = 587
 # EMAIL_HOST_USER = "tripstop756@gmail.com"
 # EMAIL_HOST_PASSWORD = "tgtbrulnxevkxtlb"
 EMAIL_HOST_USER = getenv("EMAIL_HOST_USER", "admin@stoptrip.com")
-EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD", "StopTrip(@)")
+EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD", "tfomzxifcncwbnyo")
 EMAIL_USE_TLS = True
 
 # Django location_field
