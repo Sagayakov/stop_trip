a37a7065aede71b45e8c0a241e6eb8a1b8049658
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-03-24 21:49:20 +0100

    fix: add ios image rotation

--- frontend/src/pages/addAnnouncement/libr/convertFileToBinary.ts
+++ frontend/src/pages/addAnnouncement/libr/convertFileToBinary.ts
@@ -1,122 +1,43 @@
-import EXIF from 'exif-js';
 import { convertHeicToAny } from 'features/addAnnouncementForm/universalFields/annPhoto/annPhotoField/convertHeicToAny';
 
-const fileToBase64String = (file: File, isIos: boolean) => {
+const fileToBase64String = (file: File, isIos: boolean): Promise<string> => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
-        if (isIos) {
-            (
-                EXIF.getData as unknown as (
-                    url: File,
-                    callback: () => void
-                ) => void
-            )(file, function () {
-                const orientation = EXIF.getTag(this, 'Orientation');
+        reader.onload = function (event) {
+            if (event.target?.result) {
+                if (isIos) {
+                    const img = new Image();
+                    img.onload = function () {
+                        const canvas = document.createElement('canvas');
+                        const ctx = canvas.getContext('2d');
 
-                reader.onload = function (event) {
-                    if (event.target?.result) {
-                        const img = new Image();
-                        img.onload = function () {
-                            const canvas = document.createElement('canvas');
-                            const ctx = canvas.getContext('2d');
+                        if (
+                            ctx &&
+                            /iPhone|iPad|iPod/i.test(navigator.userAgent)
+                        ) {
+                            ctx.translate(img.height, 0);
+                            ctx.rotate(Math.PI / 2);
+                        }
 
-                            if (ctx) {
-                                switch (orientation) {
-                                    case 2:
-                                        ctx.transform(
-                                            -1,
-                                            0,
-                                            0,
-                                            1,
-                                            img.width,
-                                            0
-                                        );
-                                        break;
-                                    case 3:
-                                        ctx.transform(
-                                            -1,
-                                            0,
-                                            0,
-                                            -1,
-                                            img.width,
-                                            img.height
-                                        );
-                                        break;
-                                    case 4:
-                                        ctx.transform(
-                                            1,
-                                            0,
-                                            0,
-                                            -1,
-                                            0,
-                                            img.height
-                                        );
-                                        break;
-                                    case 5:
-                                        ctx.transform(0, 1, 1, 0, 0, 0);
-                                        break;
-                                    case 6:
-                                        ctx.transform(
-                                            0,
-                                            1,
-                                            -1,
-                                            0,
-                                            img.height,
-                                            0
-                                        );
-                                        break;
-                                    case 7:
-                                        ctx.transform(
-                                            0,
-                                            -1,
-                                            -1,
-                                            0,
-                                            img.height,
-                                            img.width
-                                        );
-                                        break;
-                                    case 8:
-                                        ctx.transform(
-                                            0,
-                                            -1,
-                                            1,
-                                            0,
-                                            0,
-                                            img.width
-                                        );
-                                        break;
-                                    default:
-                                        ctx.transform(1, 0, 0, 1, 0, 0);
-                                }
-                            }
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+                        ctx?.drawImage(img, 0, 0);
+                        resolve(
+                            canvas.toDataURL('image/jpeg', 0.1).split(',')[1]
+                        );
+                    };
 
-                            canvas.width = img.width;
-                            canvas.height = img.height;
-                            ctx?.drawImage(img, 0, 0);
-                            resolve(
-                                canvas
-                                    .toDataURL('image/jpeg', 0.1)
-                                    .split(',')[1]
-                            );
-                        };
-
-                        img.src = event.target?.result as string;
-                    } else {
-                        reject(new Error('Ошибка чтения файла'));
-                    }
-                };
-            });
-        } else {
-            reader.onload = async function (event) {
-                if (event.target?.result) {
-                    const result = event.target.result as string;
-                    const base64String = result.split(',')[1];
-                    resolve(base64String);
+                    img.src = event.target?.result as string;
                 } else {
-                    reject(new Error('Ошибка чтения файла'));
+                    const base64String = (event.target.result as string).split(
+                        ','
+                    )[1];
+                    resolve(base64String);
                 }
-            };
-        }
+            } else {
+                reject(new Error('Ошибка чтения файла'));
+            }
+        };
 
         reader.onerror = function (error) {
             reject(error);
@@ -129,18 +50,27 @@ export const convertFilesToBase64Strings = async (files: File[] | FileList) => {
     const base64Strings: string[] = [];
     for (const file of files) {
         try {
-            if (file.name.includes('.HEIC') || file.name.includes('.HEIF')) {
-                const convertedFile = await convertHeicToAny(file);
-                const base64String = await fileToBase64String(
-                    convertedFile,
-                    true
-                );
-                base64Strings.push(base64String as string);
-                //});
+            let base64String = '';
+            if (
+                file.name.toLowerCase().includes('.heic') ||
+                file.name.toLowerCase().includes('.heif')
+            ) {
+                if (
+                    file.name.includes('.HEIC') ||
+                    file.name.includes('.HEIF')
+                ) {
+                    const convertedFile = await convertHeicToAny(file);
+                    base64String = await fileToBase64String(
+                        convertedFile,
+                        true
+                    );
+                } else {
+                    base64String = await fileToBase64String(file, true);
+                }
             } else {
-                const base64String = await fileToBase64String(file, false);
-                base64Strings.push(base64String as string);
+                base64String = await fileToBase64String(file, false);
             }
+            base64Strings.push(base64String as string);
         } catch (error) {
             console.error('Ошибка при чтении файла:', error);
         }
